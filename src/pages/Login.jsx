281f4b98244ad70ceb24e72849cddf7a0290f3b3
Ansh1vohra <anshvohra1@gmail.com>
2025-02-28 00:08:52 +0530

    adding Images with some changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,12 @@
 import "./pageComman.css";
+import "./Login.css";
 import vectorImg from '../assets/vectorPark2.jpg';
+import googleIcon from '../assets/gIcon.png';
 
 export default function Login() {
     return (
         <div className="min-high container-fluid d-flex justify-content-center align-items-center bg-light">
-            <div className="row w-75 shadow-lg rounded bg-white p-4">
+            <div className="row width-75 shadow-lg rounded bg-white p-4">
                 <div className="col-md-6 d-flex justify-content-center align-items-center">
                     <img src={vectorImg} alt="Vector" className="img-fluid rounded" />
                 </div>
@@ -12,11 +14,14 @@ export default function Login() {
                 <div className="col-md-6 d-flex flex-column justify-content-center">
                     <form className="p-4">
                         <h2 className="text-center mb-4 fw-bold">Sign In</h2>
+                        <button type="button" className="btn btn-dark w-100 mb-3 p-3">Sign-in with Google
+                        <img src={googleIcon} className="mx-3" alt="google" width='25px' />
+                        </button>
                         <div className="mb-3">
-                            <label className="form-label">Email Address:</label>
-                            <input type="email" className="form-control" placeholder="Enter your email" required />
+                            <label className="form-label my-2">Email Address:</label>
+                            <input type="email" className="form-control p-3" placeholder="Enter your email" required />
                         </div>
-                        <button className="btn btn-dark w-100">Send OTP</button>
+                        <button type="button" className="btn btn-dark w-100 p-3">Send OTP</button>
                     </form>
                 </div>
             </div>
